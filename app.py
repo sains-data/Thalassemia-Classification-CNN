76208cff0ba996139b4d5dd922721bd3d0912cfe
FikriDwiAlpian <100016236+Fikri109@users.noreply.github.com>
2023-12-20 19:28:54 +0700

    app.py

--- app.py
+++ app.py
@@ -1,39 +1,44 @@
 import streamlit as st
-from PIL import Image
-import numpy as np
 import json
+import logging
+import PIL
+import numpy as np
 from tensorflow.keras.models import load_model
 from sklearn.preprocessing import LabelEncoder
-from tensorflow.keras.preprocessing import image as keras_image
-from tensorflow.keras.applications.imagenet_utils import decode_predictions
+import time
 
-# Load the model and class mapping outside the ClassifyModel class
-model = load_model("model_thalassemia_classifier_80.h5")
-with open("class.json") as fin:
-    tag2class = json.load(fin)
-    class2tag = {v: k for k, v in tag2class.items()}
+logging.basicConfig(level=logging.DEBUG)
 
-# Create an instance of ClassifyModel
 class ClassifyModel:
     def __init__(self):
         self.model = None
         self.class2tag = None
         self.tag2class = None
 
-    def load(self, model_path="model_thalassemia_classifier_80.h5", class_path="class.json"):
-        self.model = load_model(model_path)
-        with open(class_path) as fin:
+    def load(self, path="/model"):
+        self.model = load_model('model_thalassemia_class.h5', compile=False)
+        with open("class.json") as fin:
             self.tag2class = json.load(fin)
             self.class2tag = {v: k for k, v in self.tag2class.items()}
+            logging.debug(f"class2tag: {self.class2tag}")
 
     def predict(self, image_array):
-        pred = self.model.predict(image_array)
-        pred_digits = np.argmax(pred, axis=1)
+        logging.debug(f"Input shape: {image_array.shape}")
+        # Ensure the input has the correct shape (None, 55, 55, 3)
+        image_array = np.expand_dims(image_array, axis=0)
+        image_array = np.expand_dims(image_array, axis=-1)  # Add a single color channel
+        image_array = np.repeat(image_array, 3, axis=-1)  # Repeat the single channel to make it 3 channels
 
-        return pred_digits, pred
+        # Use the loaded model for prediction
+        pred = self.model.predict(image_array)
+        pred_class = np.argmax(pred, axis=1)[0]
+        confidence = pred[0][pred_class]
+
+        return pred_class, confidence
 
 m = ClassifyModel()
 m.load()
+
 
 st.title('Thallasemia Classification')
 
@@ -56,25 +61,34 @@ st.sidebar.info(
     "Muhammad Nabil Azizi - 120450090"
 )
 
-uploaded_file = st.file_uploader("Choose a blood image...", type=['jpeg', 'jpg', 'png'])
+uploaded_file = st.file_uploader("", type=['jpeg', 'jpg', 'png'])
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file).resize((80, 80))
+    # Preprocess the uploaded image
+    image = PIL.Image.open(uploaded_file).resize((100, 100))
     img_array = np.array(image)
-    img_array = img_array / 255.0
-    img_array = np.expand_dims(img_array, axis=0)
-    img_array = np.expand_dims(img_array, axis=-1) 
-    img_array = np.repeat(img_array, 3, axis=-1)
+    img_array = img_array / 255  # Normalize pixel values to the range [0, 1]
 
+    # Display the uploaded image
     st.image(image, use_column_width=True, caption=f'Uploaded Image: {uploaded_file.name}')
 
+    # Button to initiate prediction
     if st.button('Predict'):
-        pred_digits, pred_probabilities = m.predict(img_array)
+        # Record the start time
+        start_time = time.time()
 
-        if len(pred_digits) > 0 and pred_digits[0] < len(class2tag):
-            predicted_label = class2tag[pred_digits[0]]
-            confidence = pred_probabilities[0][pred_digits[0]]
-            st.write(f"Predicted Label: {predicted_label}")
-            st.write(f"Confidence: {round(float(confidence), 4) * 100}%")
-        else:
-            st.write("Error: Predicted class index out of bounds.")
+        # Use the model for prediction
+        predicted_class, confidence = m.predict(img_array)
+
+        # Record the end time
+        end_time = time.time()
+
+        # Get the predicted label
+        predicted_label = m.class2tag[predicted_class]
+
+        # Display the prediction result
+        st.write(f"This is **{predicted_label}** (confidence: **{round(float(confidence), 4) * 100}%**)")
+        
+        # Display the execution time
+        execution_time = end_time - start_time
+        st.write(f"Time taken for prediction: {execution_time:.4f} seconds")
